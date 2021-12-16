6222879c811745833f63d42042fff5ce70c6760e
Iago111996 <iago111996@gmail.com>
2021-12-16 14:13:09 -0300

    add page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
@@ -7,7 +8,24 @@ export default function Home() {
     <Head>
     <title>Inicio | ig.news</title>
     </Head>
-    <h1 className={styles.title}>Hello World</h1> 
+    
+    <main className={styles.container}>
+      <section className={styles.hero}>
+        <span>👏  Hey, welcome</span>
+
+        <h1>News about the <span>React</span> world</h1>
+
+        <p>
+          Get access to all the publication <br />
+
+          <span>for $9.9 month</span>
+        </p>
+
+        <SubscribeButton />
+      </section>
+
+      <img src="images/avatar.svg" alt="Girl coding" />
+    </main>
    </>
   )
 }
