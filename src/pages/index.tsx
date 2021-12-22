c5b4ce6a73db9b5cd2e2c599be01b6576a2abc06
Iago111996 <iago111996@gmail.com>
2021-12-22 10:38:47 -0300

    add stripe-api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,61 @@
-import Head from 'next/head';
-import { SubscribeButton } from '../components/SubscribeButton';
-import styles from '../styles/home.module.scss';
+import Head from "next/head";
+import { GetStaticProps } from "next";
 
-export default function Home() {
+import styles from "../styles/home.module.scss";
+
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+
+interface HomeProps {
+  product: { priceId: string; amount: number };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
-    <Head>
-    <title>Inicio | ig.news</title>
-    </Head>
-    
-    <main className={styles.container}>
-      <section className={styles.hero}>
-        <span>👏  Hey, welcome</span>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
 
-        <h1>News about the <span>React</span> world</h1>
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
 
-        <p>
-          Get access to all the publication <br />
+          <h1>
+            News about the <span>React</span> world
+          </h1>
 
-          <span>for $9.9 month</span>
-        </p>
+          <p>
+            Get access to all the publication <br />
+            <span>for {product.amount} month</span>
+          </p>
 
-        <SubscribeButton />
-      </section>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
 
-      <img src="images/avatar.svg" alt="Girl coding" />
-    </main>
-   </>
-  )
+        <img src="images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1K9TBZBFMh7qhQoQsqLbKS83", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    redirect: 60 *60 * 24, //24 hours
+  };
+};
